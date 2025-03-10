30f977985eecaf928bd86de940eab5db4a95453c
antoineleboucher <antwanlbr.work@gmail.com>
2025-03-10 09:29:17 +0100

    initialized tailwind

---
+++ coach-2B-5-frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",  // Scan tous les fichiers HTML et TypeScript dans src/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
